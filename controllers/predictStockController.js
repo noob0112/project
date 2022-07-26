2e4ccee0657b0dde098af74d56612d31ba5ad9d3
Nguyen Viet Hoang <hoang011220@gmail.com>
2022-07-26 19:46:25 +0700

    update

--- controllers/predictStockController.js
+++ controllers/predictStockController.js
@@ -2,45 +2,42 @@ const PredictStock = require("../models/PredictStock");
 
 const readAll = async (req, res) => {
     if (!req.query.symbol)
-        await PredictStock.find({})
-            .then((stocks) => {
-
-                return res.status(200).json(stocks.map(stock => {
-                    return {
-                        _id: stock._id,
-                        symbol: stock.symbol,
-                        open: stock.Open,
-                        close: stock.Close,
-                        low: stock.Low,
-                        high: stock.High,
-                        date: stock.Date
-                    }
-                }));
-            })
-            .catch((error) => {
+        try {
+            const stocks = await PredictStock.find({}).catch((error) => {
                 return res.status(404).json({ message: "Stocks are non-existence", error });
             });
+            return res.status(200).json(stocks.map(stock => {
+                return {
+                    _id: stock._id,
+                    symbol: stock.symbol,
+                    open: stock._doc.Open,
+                    close: stock._doc.Close,
+                    low: stock._doc.Low,
+                    high: stock._doc.High,
+                    date: stock._doc.Date
+                }
+            }))
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
 
     let symbol = req.query.symbol
 
     try {
-        await PredictStock.find({ symbol: symbol })
-            .then((stocks) => {
-                return res.status(200).json(stocks.map(stock => {
-                    return {
-                        _id: stock._id,
-                        symbol: stock.symbol,
-                        open: stock.Open,
-                        close: stock.Close,
-                        low: stock.Low,
-                        high: stock.High,
-                        date: stock.Date
-                    }
-                }));
-            })
-            .catch((error) => {
-                return res.status(404).json({ message: "Stocks are non-existence", error });
-            });
+        const stocks = await PredictStock.find({ symbol: symbol }).catch((error) => {
+            return res.status(404).json({ message: "Stocks are non-existence", error });
+        });
+        return res.status(200).json(stocks.map(stock => {
+            return {
+                _id: stock._id,
+                symbol: stock.symbol,
+                open: stock.Open,
+                close: stock.Close,
+                low: stock.Low,
+                high: stock.High,
+                date: stock.Date
+            }
+        }))
     } catch (error) {
         return res.status(500).json(error);
     }
